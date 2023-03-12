ec15b25c8079681946fbcd1b108a0a307030760e
Vladislav Korenkov <vladnfs3@gmail.com>
2023-03-13 00:36:32 +1000

    Complete fix

--- ontology/models/algorithms.py
+++ ontology/models/algorithms.py
@@ -5,45 +5,58 @@ from ontology.models.models import Area, Route, Truck, Cargo, ComputedRoute
 logger = logging.getLogger(__name__)
 
 
-def dijkstra(area_from: Area) -> dict:
-    distances = {}
-    paths = {}
+def dijkstra(area_from: Area) -> tuple[dict, dict]:
+    distances = {None: float('inf')}
+    parents = {}
     checked = {}
     area_ids = Area.objects.values_list('id', flat=True)
 
     for area_id in area_ids:
         distances[area_id] = float('inf')
-        paths[area_id] = []
+        parents[area_id] = None
         checked[area_id] = False
 
     distances[area_from.id] = 0
 
-    for area in Area.objects.all():
+    for _ in area_ids:
         current_area_id = None
 
         for looking_area_id in area_ids:
             if not checked[looking_area_id] and (current_area_id is None \
                     or distances[looking_area_id] < distances[current_area_id]):
                 current_area_id = looking_area_id
-            else:
-                continue
 
             if distances[current_area_id] == float('inf'):
                 continue
 
             checked[current_area_id] = True
 
+            area = Area.objects.get(id=current_area_id)
             for route in area.wherefrom_routes.union(area.whereto_routes.all()):
                 route: Route
                 distance = distances[current_area_id] + route.distance
                 if distance < distances[route.whereto.id]:
                     distances[route.whereto.id] = distance
-                    paths[route.whereto.id] += [current_area_id]
+                    parents[route.whereto.id] = current_area_id
+                if distance < distances[route.wherefrom.id]:
+                    distances[route.wherefrom.id] = distance
+                    parents[route.wherefrom.id] = current_area_id
 
-    for area_id in area_ids:
-        paths[area_id] += [area_id]
+    return distances, parents
 
-    return distances, paths
+
+def restore_path(area_from: int, area_to: int, parents: dict) -> list[int]:
+    if area_from == area_to:
+        return []
+
+    current_area_id = area_to
+    path = []
+    while current_area_id != area_from:
+        path += [current_area_id]
+        current_area_id = parents[current_area_id]
+
+    path += [area_from]
+    return list(reversed(path))
 
 
 def compute_routes():
@@ -65,18 +78,13 @@ def compute_routes():
             truck: Truck
             computed_route_kwargs = {'cargo': cargo, 'truck': truck}
 
-            truck_to_cargo_distances, truck_to_cargo_paths \
-                = dijkstra(truck.location)
-            truck_to_cargo_distance, truck_to_cargo_path = (
-                truck_to_cargo_distances[cargo.location.id],
-                truck_to_cargo_paths[cargo.location.id],
-            )
-            cargo_to_destination_distances, cargo_to_destination_paths = \
+            truck_to_cargo_distances, truck_to_cargo_parents = dijkstra(truck.location)
+            truck_to_cargo_distance = \
+                truck_to_cargo_distances[cargo.location.id]
+            cargo_to_destination_distances, cargo_to_destination_parents = \
                 dijkstra(cargo.location)
-            cargo_to_destination_distance, cargo_to_destination_path = (
-                cargo_to_destination_distances[cargo.destination.id],
-                cargo_to_destination_paths[cargo.destination.id],
-            )
+            cargo_to_destination_distance = \
+                cargo_to_destination_distances[cargo.destination.id]
 
             computed_route_kwargs['distance'] = \
                 truck_to_cargo_distance \
@@ -85,9 +93,15 @@ def compute_routes():
                 computed_route_kwargs['distance'] \
                 * truck.model.fuel_consumption \
                 / 100
-            computed_route_kwargs['path'] = \
-                truck_to_cargo_path[:-1] \
-                + cargo_to_destination_path
+            computed_route_kwargs['path'] = restore_path(
+                truck.location.id,
+                cargo.location.id,
+                truck_to_cargo_parents,
+            )[:-1] + restore_path(
+                cargo.location.id,
+                cargo.destination.id,
+                cargo_to_destination_parents,
+            )
 
             logger.info(f'Computed route with kwargs: {computed_route_kwargs}')
             computed_routes.append(ComputedRoute(**computed_route_kwargs))
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from django.db.utils import IntegrityError
 from vstutils.tests import BaseTestCase
 from ontology.models import models
+from ontology.constants import CargoType
 
 
 class ConstraintsTestCase(BaseTestCase):
@@ -103,3 +104,166 @@ class ConstraintsTestCase(BaseTestCase):
                 whereto=self.area1,
                 distance=1,
             )
+
+
+class RouteComputingTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.truck_model1 = models.TruckModel.objects.create(
+            name='truck_model1',
+            load_capacity=100,
+            gas_tank_capacity=100,
+            fuel_consumption=10,
+        )
+
+    def make_calculations(self):
+        self.bulk_transactional([
+            {'method': 'post', 'path': ['computed_routes', 'compute_routes']},
+        ])
+
+    def get_calculated_routes_list(self):
+        results = self.bulk_transactional([
+            {'method': 'get', 'path': 'computed_routes'},
+        ])
+        return results[0]['data']['results'], results[0]['data']['count']
+
+    def get_calculated_route(self, route_id):
+        results = self.bulk_transactional([
+            {'method': 'get', 'path': ['computed_routes', route_id]},
+        ])
+        return results[0]['data']
+
+    def test_simple_1(self):
+        area1 = models.Area.objects.create(name='area1')
+        area2 = models.Area.objects.create(name='area2')
+
+        models.Route.objects.create(
+            wherefrom=area1,
+            whereto=area2,
+            distance=10,
+        )
+
+        truck1 = models.Truck.objects.create(
+            model=self.truck_model1,
+            number='truck1',
+            type=CargoType.Bulk,
+            location=area1,
+        )
+        cargo1 = models.Cargo.objects.create(
+            type=CargoType.Bulk,
+            location=area1,
+            destination=area2,
+            mass=100,
+        )
+
+        # create cargo which cannot be delivered
+        models.Cargo.objects.create(
+            type=CargoType.Filler,
+            location=area2,
+            destination=area1,
+            mass=90,
+        )
+        models.Cargo.objects.create(
+            type=CargoType.Bulk,
+            location=area2,
+            destination=area1,
+            mass=1000,
+        )
+        # create not suitable trucks
+        models.Truck.objects.create(
+            model=self.truck_model1,
+            number='truck2',
+            type=CargoType.Peace,
+            location=area1,
+        )
+
+        self.make_calculations()
+        list_result, count = self.get_calculated_routes_list()
+        self.assertEqual(count, 1)
+        result = self.get_calculated_route(list_result[0]['id'])
+        self.assertDictEqual(result, {
+            **result,
+            'truck': truck1.id,
+            'cargo': cargo1.id,
+            'distance': 10,
+            'fuel': 1,
+            'path': [area1.id, area2.id],
+        })
+
+    def test_multiple_areas_delivery(self):
+        area1 = models.Area.objects.create(name='area1')
+        area2 = models.Area.objects.create(name='area2')
+        area3 = models.Area.objects.create(name='area3')
+        area4 = models.Area.objects.create(name='area4')
+
+        models.Route.objects.create(
+            wherefrom=area1,
+            whereto=area2,
+            distance=10,
+        )
+        models.Route.objects.create(
+            wherefrom=area2,
+            whereto=area3,
+            distance=10,
+        )
+        models.Route.objects.create(
+            wherefrom=area3,
+            whereto=area4,
+            distance=10,
+        )
+
+        truck1 = models.Truck.objects.create(
+            model=self.truck_model1,
+            number='truck1',
+            type=CargoType.Bulk,
+            location=area1,
+        )
+        cargo1 = models.Cargo.objects.create(
+            type=CargoType.Bulk,
+            location=area1,
+            destination=area4,
+            mass=100,
+        )
+
+        truck2 = models.Truck.objects.create(
+            model=self.truck_model1,
+            number='truck2',
+            type=CargoType.Filler,
+            location=area3,
+        )
+        cargo2 = models.Cargo.objects.create(
+            type=CargoType.Filler,
+            location=area1,
+            destination=area4,
+            mass=100,
+        )
+
+        self.make_calculations()
+        list_result, count = self.get_calculated_routes_list()
+        self.assertEqual(count, 2)
+        result = self.get_calculated_route(list_result[0]['id'])
+        self.assertDictEqual(result, {
+            **result,
+            'truck': truck1.id,
+            'cargo': cargo1.id,
+            'distance': 30.0,
+            'fuel': 3.0,
+            'path': [area1.id, area2.id, area3.id, area4.id],
+        })
+
+        result = self.get_calculated_route(list_result[1]['id'])
+        self.assertDictEqual(result, {
+            **result,
+            'truck': truck2.id,
+            'cargo': cargo2.id,
+            'distance': 50.0,
+            'fuel': 5.0,
+            'path': [
+                area3.id,
+                area2.id,
+                area1.id,
+                area2.id,
+                area3.id,
+                area4.id,
+            ],
+        })
